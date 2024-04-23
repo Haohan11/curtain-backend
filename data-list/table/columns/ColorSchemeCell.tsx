05dc90501b3b64f1e348e8ae842f899f05ee3a66
haohan <han.chang@i2csolution.com>
2024-04-23 18:48:41 +0800

    render cells only props is array

--- data-list/table/columns/ColorSchemeCell.tsx
+++ data-list/table/columns/ColorSchemeCell.tsx
@@ -1,6 +1,6 @@
 const ColorSchemeCell = ({ colorScheme }) => (
   <div>
-    {colorScheme.map(scheme => <div className="badge badge-light-primary me-1" key={scheme.id}>{scheme.name}</div>)}
+    {Array.isArray(colorScheme) && colorScheme.map(scheme => <div className="badge badge-light-primary me-1" key={scheme.id}>{scheme.name}</div>)}
   </div>
 )
 
--- data-list/table/columns/ColorsCell.tsx
+++ data-list/table/columns/ColorsCell.tsx
@@ -1,6 +1,6 @@
 const ColorsCell = ({ colorList }) => (
   <div className="d-flex flex-column">
-    {colorList.map(color => <div className="badge badge-secondary mb-2 align-self-start" key={color.id}>{color.name}</div>)}
+    {Array.isArray(colorList) && colorList.map(color => <div className="badge badge-secondary mb-2 align-self-start" key={color.id}>{color.name}</div>)}
   </div>
 )
 
