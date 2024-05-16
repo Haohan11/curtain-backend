e179a4766ed8735001183a1c613b57078fd4e5c2
Roy You <roy.you@i2csolution.com>
2024-05-16 18:26:18 +0800

    修正按鈕第一次進入無效問題

--- data-list/table/columns/ActionsCell.tsx
+++ data-list/table/columns/ActionsCell.tsx
@@ -6,6 +6,9 @@ import {ID, KTIcon, QUERIES} from '@/_metronic/helpers'
 import {useListView} from '../../core/ListViewProvider'
 import {useQueryResponse} from '../../core/QueryResponseProvider'
 import {deleteUser} from '../../core/_requests'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 type Props = {
   id: ID
@@ -31,6 +34,20 @@ const ActionsCell: FC<Props> = ({id}) => {
 
   return (
     <>
+      <DropdownButton
+        size="sm"
+        variant="secondary"
+        title="操作"
+        id={`dropdown-button-drop-0`}
+        key={0}
+      >
+        {/* <Dropdown.Item eventKey="1">Action</Dropdown.Item> */}
+        <Dropdown.Item  onClick={openEditModal} >編輯</Dropdown.Item>
+        {/* <Dropdown.Item eventKey="3">Something else here</Dropdown.Item> */}
+        {/* <Dropdown.Divider /> */}
+        {/* <Dropdown.Item eventKey="4">Separated link</Dropdown.Item> */}
+      </DropdownButton>
+      {/*       
       <a
         href='#'
         className='btn btn-light btn-active-light-primary btn-sm'
@@ -40,21 +57,22 @@ const ActionsCell: FC<Props> = ({id}) => {
         操作
         <KTIcon iconName='down' className='fs-5 m-0' />
       </a>
-      {/* begin::Menu */}
+
       <div
         className='menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4'
         data-kt-menu='true'
       >
-        {/* begin::Menu item */}
         <div className='menu-item px-3'>
           <a className='menu-link px-3' onClick={openEditModal}>
             編輯
           </a>
         </div>
-        {/* end::Menu item */}
-
-        {/* begin::Menu item */}
-        {/* <div className='menu-item px-3'>
+        
+      </div>      
+      */}
+    </>
+  )
+  {/* <div className='menu-item px-3'>
           <a
             className='menu-link px-3'
             data-kt-users-table-filter='delete_row'
@@ -63,11 +81,6 @@ const ActionsCell: FC<Props> = ({id}) => {
             刪除
           </a>
         </div> */}
-        {/* end::Menu item */}
-      </div>
-      {/* end::Menu */}
-    </>
-  )
 }
 
 export {ActionsCell}
