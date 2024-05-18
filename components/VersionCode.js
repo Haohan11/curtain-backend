ab301f8b0f5925eb3ba4028cf4356fa1aa9ec1e0
han <han.chang@i2csolution.com>
2024-05-18 17:22:40 +0800

    add version code in login page

---
+++ components/VersionCode.js
@@ -0,0 +1,9 @@
+const versionText = "1.0"
+
+const VersionCode = () => (
+  <span className="position-fixed py-2 px-3 bottom-0 end-0 fs-6-xs text-textgrey">
+    版本號: {versionText}
+  </span>
+);
+
+export default VersionCode;
--- components/loginPageLayout.js
+++ components/loginPageLayout.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Row, Col as BSCol, Container } from "react-bootstrap";
 import addClassName from "@/tool/addClassName";
 
+import VersionCode from "./VersionCode";
+
 const Col = addClassName(BSCol, "p-0");
 
 const LoginPageLayout = ({ content }) => {
 
   return (
+    <>
     <Row className="g-0">
       <Col>
         <div className="vh-100 px-4 px-lg-6 flex-column">
@@ -16,11 +19,13 @@ const LoginPageLayout = ({ content }) => {
             {content}
           </div>
           <div className="flex-center" style={{ height: "10vh" }}>
-            <p className="text-textgrey">Copyright © 2024 XiangYu Drapery. All rights reserved.</p>
+            <p className="" style={{color: "grey"}}>Copyright © 2024 XiangYu Drapery. All rights reserved.</p>
           </div>
         </div>
       </Col>
     </Row>
+    <VersionCode />
+    </>
   );
 };
 
