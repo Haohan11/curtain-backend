72c691c6483e8b9a28a4d18d8063d609481a9baa
haohan <han.chang@i2csolution.com>
2024-06-04 11:18:56 +0800

    Add host ip in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
+        hostname: "35.236.145.210",
+        // port: "3005"
+      },
+      {
+        protocol: "http",
         hostname: "localhost",
         // port: "3005"
       },
