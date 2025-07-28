ca847cd1c98b43d2b75079b4364d6438f137cbbf
HaoHan <haohan11pfer@protonmail.com>
2025-07-29 01:38:26 +0800

    Change hostname to node-server

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "35.236.145.210",
-        // port: "3005"
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        // port: "3005"
-      },
+        hostname: "node-server",
+      }
     ],
   },
   eslint: {
